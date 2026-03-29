581f744b10458328c7f9b06b6b42e195229965fb
Julia Czyzewska <julaczyzewska28@gmail.com>
2026-03-29 21:17:57 +0200

    feat: added stage 3, 4 of populating db - events and participants

--- scripts/populate_db.py
+++ scripts/populate_db.py
@@ -7,7 +7,8 @@ data_files_paths = {
     "venues": "seeds/venues.json",
     "entities": "seeds/entities.json",
     "stages": "seeds/stages.json",
-   
+    "events": "seeds/events.json",
+    "participants": "seeds/participants.json",
 }
 
 def load_table_data_from_json(table_name: str):
@@ -51,14 +52,32 @@ def populate_entites(cur):
     """, [(d.get("type"), d.get("name"), d.get("official_name"), d.get("slug"), d.get("abbreviation"), d.get("_country_abr")) for d in data])
 
 
-
-
 def populate_stages(cur):
     data = load_table_data_from_json("stages")
     cur.executemany("""
         INSERT OR IGNORE INTO stages (_competition_slug, name, ordering)
         VALUES (?, ?, ?)
     """, [(d.get("_competition_slug"), d.get("name"), d.get("ordering")) for d in data])
+
+
+def populate_events(cur):
+    data = load_table_data_from_json("events")
+    cur.executemany("""
+        INSERT OR IGNORE INTO events (status, season, date_venue, time_venue_UTC, _stage_id, _venue_id, _competition_slug)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+    """, [(d.get("status"), d.get("season"), d.get("date_venue"), d.get("time_venue_UTC"), d.get("_stage_id"), d.get("_venue_id"), d.get("_competition_slug")) for d in data])
+
+
+
+def populate_participants(cur):
+    data = load_table_data_from_json("participants")
+    cur.executemany("""
+        INSERT OR IGNORE INTO participants (_event_id, _entity_id, role, stage_position)
+        VALUES (?, ?, ?, ?)
+    """, [(d.get("_event_id"), d.get("_entity_id"), d.get("role"), d.get("stage_position")) for d in data])
+
+
+
 
 
 def populate_db():
@@ -76,7 +95,18 @@ def populate_db():
         populate_stages(cur)
         conn.commit()
 
-        
+        # stage 3 - event
+        populate_events(cur)
+        conn.commit()
+
+        # stage 4 - participant
+        populate_participants(cur)
+        conn.commit()
+
+
+        # stage 5 - even incident, participant score, event score
+
+
 
 
 
